7b1cd435cb7ae079cf519236e8e9ef08128e7b3d
Viktarios <missvika7066@gmail.com>
2023-04-18 21:09:05 +0300

    script

--- lesson25/task06.py
+++ lesson25/task06.py
@@ -1,5 +1,7 @@
 students = {"Alex": 10, "Peter": 9, "Victor": 7,
             "Anna": 8, "Kate": 8, "Vladimir": 10, "Nikita": 8}
-print("Before: ", students)
-students.cl
-print("After: ", students)
+
+# print(len(students))
+
+for key in students:
+    print(key + " - " + str(students[key]))
