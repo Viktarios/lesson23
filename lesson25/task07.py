e277ee556ffd310a0789bd90754520dcda3e3d95
Viktarios <missvika7066@gmail.com>
2023-04-18 21:43:18 +0300

    script

---
+++ lesson25/task07.py
@@ -0,0 +1,5 @@
+students = {"Alex": 10, "Peter": 9, "Victor": 7,
+            "Anna": 8, "Kate": 8, "Vladimir": 10, "Nikita": 8}
+
+for key,value in students.items():
+    print(key," - ", value)
---
+++ lesson25/task10.py
@@ -0,0 +1,10 @@
+# empty_dict1 = {}
+# empty_dict2 = dict()
+# # d1 = dict([(1,0), (2,0), (3,0), (4,0), (5,0)])
+# d1 = dict.fromkeys("abcde",0)
+# print(d1)
+import  string
+
+print(string.ascii_uppercase)
+print(string.ascii_lowercase)
+print(string.ascii_letters)
