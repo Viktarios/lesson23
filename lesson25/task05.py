4f7fd67dd0a33fca27939fb4c1cdfe9a698cf4b3
Viktarios <missvika7066@gmail.com>
2023-04-18 20:24:02 +0300

    Dictionary script

--- lesson25/task05.py
+++ lesson25/task05.py
@@ -1,6 +1,5 @@
-
 students = {"Alex": 10, "Peter": 9, "Victor": 7,
             "Anna": 8, "Kate": 8, "Vladimir": 10, "Nikita": 8}
 print("Before: ", students)
-students["Alice"] = 5
+students.setdefault("Alice", 7)
 print("After: ", students)
