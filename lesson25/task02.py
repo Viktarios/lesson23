e4741d05df449bf898843d6f93b085895d5baaf3
Viktarios <missvika7066@gmail.com>
2023-04-18 20:06:19 +0300

    Dictionary script

---
+++ lesson25/task02.py
@@ -0,0 +1,12 @@
+students = ["Alex", "Peter", "Victor",
+            "Anna", "Kate", "Vladimir", "Nikita"]
+
+marks = [10, 9, 7, 8, 8, 10, 8]
+
+# index = students.index("Victor")
+# print(marks[index])
+
+dictionary = {"Alex": 10, "Peter": 9, "Victor": 7,
+              "Anna": 8, "Kate": 8, "Vladimir": 10, "Nikita": 8}
+
+print(dictionary["Kate"])
