bbe5d88a4ec744c111b36d9a47a4263334215cad
Viktarios <missvika7066@gmail.com>
2023-04-18 20:31:29 +0300

    Dictionary script

---
+++ lesson25/task04.py
@@ -0,0 +1,5 @@
+students = {"Alex": 10, "Peter": 9, "Victor": 7,
+            "Anna": 8, "Kate": 8, "Vladimir": 10, "Nikita": 8}
+print("Before: ", students)
+del students ["Victor"]
+print("After: ", students)
--- lesson25/task05.py
+++ lesson25/task05.py
@@ -1,5 +1,5 @@
 students = {"Alex": 10, "Peter": 9, "Victor": 7,
             "Anna": 8, "Kate": 8, "Vladimir": 10, "Nikita": 8}
 print("Before: ", students)
-students.setdefault("Alice", 7)
+students.update({"Alice":7,"Eliza":9})
 print("After: ", students)
