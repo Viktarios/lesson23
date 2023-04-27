6a69ad0f08968024602a73946698e90763166c9e
Viktarios <missvika7066@gmail.com>
2023-04-27 20:31:44 +0300

    using private fields

--- lesson26/bus.py
+++ lesson26/bus.py
@@ -1,11 +1,20 @@
 class Bus:
-    def __init__(self, brand='no name', price=0, number=0):
-        self.brand = brand
-        self.price = price
-        self.number = number
+    def __init__(self, brand='no name', price=0, number=[]):
+        self.__brand = brand
+        self.__price = price
+        self.__number = number
 
-    # def __del__(self):
-    #     print(f"destructor for {self.brand}")
+    def get_brand(self):
+        return self.__brand
+
+    def set_brand(self, name):
+        self.__brand = name
+
+    def get_number(self):
+        return self.__number
+
+    def set_number(self):
+        return self.__number
 
     def __str__(self):
         return f"Bus: {self.brand}, price =${self.price}, " \
@@ -13,9 +22,5 @@ class Bus:
 
 
 if __name__ == "__main__":
-    bus1 = Bus("Scania")
-    bus2 = Bus("MAZ")
-    bus3 = Bus("Volvo")
-    bus4 = Bus("PAZ")
-    bus5 = Bus("Mercedes Benz")
-
+    bus = Bus()
+    print(bus.)
---
+++ lesson26/test.py
@@ -0,0 +1,7 @@
+from bus import *
+
+
+if __name__ == "__main__":
+    b = bus.Bus()
+    bus._number = -10
+    print(bus)
