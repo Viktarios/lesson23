fba9b21ae406235303ff98952aadcb936e8204fe
Viktarios <missvika7066@gmail.com>
2023-05-18 21:50:39 +0300

    Use special methods setUp and tearDown

---
+++ lesson31/task03/calculator_test.py
@@ -0,0 +1,53 @@
+import unittest
+from calculator import Calculator
+
+
+class CalculatorTaste(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = Calculator()
+
+    def tearDown(self):
+        del self.calc
+
+    def testAddPositive(self):
+        a = 5
+        b = 6
+        expected = 11
+
+        actual = self.calc.add(a, b)
+
+        self.assertEqual(expected, actual)
+
+    def testSubPositive(self):
+        a = 5
+        b = 6
+        expected = -1
+
+
+        actual = self.calc.sub(a, b)
+
+        self.assertEqual(expected, actual)
+
+    def testMulPositive(self):
+        a = 5
+        b = 6
+        expected = 30
+
+        actual = self.calc.mul(a, b)
+
+        self.assertEqual(expected, actual)
+
+    def testDivPositive(self):
+        a = 10
+        b = 2
+        expected = 5
+
+        actual = self.calc.div(a, b)
+
+        self.assertEqual(expected, actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
